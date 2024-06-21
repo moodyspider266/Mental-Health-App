2b736361b0c7d456f83da8991a8972b51623dfd7
Kushl <kushlalve007@gmail.com>
2024-06-21 17:49:19 +0530

    Modified sidebar and css along with .env in server

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -3,11 +3,11 @@ import "../components_styling/Sidebar.css";
 
 const Sidebar = () => {
   const options = [
-    { name: 'Dashboard', icon: 'dashboard', link: '/dashboard' },
+    { name: 'Dashboard', icon: 'dashboard', link: '/' },
     { name: 'Community', icon: 'community', link: '/community' },
-    { name: 'Surveys', icon: 'surveys', link: '/surveys' },
-    { name: 'To-do-List', icon: 'to-do-list', link: '/to-do-list' },
     { name: 'Recommendations', icon: 'recommendations', link: '/recommendations' },
+    { name: 'To-do-List', icon: 'to-do-list', link: '/to-do-list' },
+    { name: 'Surveys', icon: 'surveys', link: '/surveys' },
     { name: 'Settings', icon: 'settings', link: '/settings' },  
   ];
 
