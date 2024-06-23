92e1827dee144e8fd2058e8f81c3f6543868f232
Kushl <kushlalve007@gmail.com>
2024-06-23 15:34:36 +0530

    working on main-content

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,47 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <div className="home-container">
-      <div className="my-cards">
-        {/* My Cards component */}
-        <div className="card">
-          Card 1 Content
-        </div>
-        <div className="card">
-          Card 2 Content
-        </div>
-      </div>
-      
-      <div className="recent-transactions">
-        {/* Recent Transactions component */}
-        <ul>
-          <li>Transaction 1</li>
-          <li>Transaction 2</li>
-          <li>Transaction 3</li>
-        </ul>
-      </div>
-      
-      <div className="weekly-activity-chart">
-        {/* Weekly Activity Chart component */}
-        {/* Bar diagram for each day of the week */}
-      </div>
-      
-      <div className="expense-statistics">
-        {/* Expense Statistics component */}
-        {/* Pie chart showing distribution of amount spent in different categories */}
-      </div>
-      
-      <div className="quick-transfer">
-        {/* Quick Transfer component */}
-        {/* Three users at the start */}
-        {/* Button to show next three users */}
-        {/* Input for amount to be transferred */}
-        {/* Send button */}
-      </div>
-      
-      <div className="balance-history">
-        {/* Balance History component */}
-        {/* Diagram showing monthly spending */}
-      </div>
+      <h1>Home</h1>
     </div>
   );
 }
