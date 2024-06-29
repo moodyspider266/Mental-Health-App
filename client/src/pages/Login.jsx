fdeb12b43e1c8af0244072653b6f7525cab6dba8
Veda19g <vedang19gambhire@gmail.com>
2024-06-29 12:58:20 +0530

    Y

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,24 @@
 import React from "react";
-
+import "./../components_styling/Login.css"
 export default function Login() {
     return (
         <div>
-            <h1>Login</h1>
+            <div className="container">
+              <div className="section-1">
+                <h2>Login</h2>
+                <form className="form">
+                    <div>
+                    <label>Email</label>
+                    <input type="email" name="email" placeholder="Enter your email" />
+                    </div>
+                    <div>
+                     <label>Password</label>
+                     <input type="password" name="password" placeholder="Enter your password" />
+                    </div>
+                    <button>Login</button>
+                </form>
+              </div>
+            </div>
         </div>
     );
 }
